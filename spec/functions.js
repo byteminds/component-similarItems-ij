d3b4a6f39a89f7019d7eeceabf03c34d3c1599fe
Ivar Johnson <m.ivar.johnson@gmail.com>
2019-06-28 16:22:21 -0700

    more tests added

--- spec/functions.js
+++ spec/functions.js
@@ -1,11 +1,36 @@
 const axios = require('axios');
+const getType = require('jest-get-type');
 
 const functions = {
   fetchItems: () => axios.get('http://localhost:3002/similaritems/46')
     .then(res => res.data)
     .catch(err => {
-      console.log('ERROR!', err);
+      console.log(' >>>>>>>>>>> ERROR! ', err);
+    }),
+
+  dataType: () => axios.get('http://localhost:3002/similaritems/46')
+    .then(res => {
+      let result = res.data;
+      result = getType(result);
+      console.log(result);
+      return result;
     })
+    .catch(err => {
+      console.log(' dataType function error: ', err);
+    }),
+
+  itemsType: () => axios.get('http://localhost:3002/similaritems/46')
+    .then(res => {
+      let result = res.data.items;
+      result = getType(result);
+      console.log(result);
+      return result;
+    })
+    .catch(err => {
+      console.log(' dataType function error: ', err);
+    }),
+
+
 }
 
 module.exports = functions;
--- spec/test.js
+++ spec/test.js
@@ -3,15 +3,30 @@ const functions = require('./functions');
 describe('`similaritems` API calls by ID', () => {
 
   test('Should fetch `relatedTo` from DB by ID', () => {
-    expect.assertions(1);
     return functions.fetchItems()
       .then(data => expect(data.items.length).toBeGreaterThan(0))
   });
 
   test('Items fetched from DB should number <= to 6', () => {
-    expect.assertions(1);
     return functions.fetchItems()
       .then(data => expect(data.items.length).toBeLessThanOrEqual(6))
+  });
+
+  test('return data from api call should be type of `object`', () => {
+    return functions.dataType()
+      .then(data => expect(data).toMatch('object'))
+  });
+
+  test('return `items` from api call should be type of `array`', () => {
+    return functions.itemsType()
+      .then(data => expect(data).toMatch('array'))
+  });
+
+  test('return item from api call should have relatedTo ID that matches call', () => {
+    return functions.fetchItems()
+      .then(data => {
+        expect(data.items[0].relatedTo).toBe(46)
+      })
   });
 
 });
